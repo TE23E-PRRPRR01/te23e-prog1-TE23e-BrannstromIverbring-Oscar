98a2ecd081027e27f008d050860b5aa42441d0cc
Oscar Brännström Iverbring <oscarbrannstromiverbring@elev.ga.ntig.se>
2024-08-26 14:46:18 +0200

    tränat på if

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+
+Console.Clear();
+
+Console.WriteLine("Hej Världen!");
+Console.WriteLine("Nu mitt första C#-program");
+
+Console.WriteLine("Vad heter du?");
+string namn = Console.ReadLine();
+Console.WriteLine("Hej " + namn);
+Console.WriteLine("Vad gillar du på fritiden? " + namn);
+string fritiden = Console.ReadLine();
+Console.WriteLine("Vad roligt!");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Jag älskar Kendrick Lamar");
---
+++ Kapitel 1/Registrering/Program.cs
@@ -0,0 +1,25 @@
+﻿// Program för att registera förnamn och efternamn
+using System.Runtime.CompilerServices;
+
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode; 
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Program för att registera förnamn och efternamn");
+Console.WriteLine("Ange Förnamn");
+string förnamn = Console.ReadLine();
+Console.WriteLine("Ange Efternamn");
+string efternamn = Console.ReadLine();
+Console.WriteLine("Hej! " + förnamn + " " + efternamn);
+Console.WriteLine($"Hej! {förnamn} {efternamn}");
+
+//Är det Oscar som loggar in?
+if förnamn == "Oscar")
+{
+    Console.WriteLine("Vad trevligt att du är tillbaka!");
+}
+
+if efternamn == "Brännström Iverbring")
+{
+    Console.WriteLine(Vackert E
+    fternamn!);
+}
