9d4d7c204b1f680983c1a8a1ecec84ae4492309d
Oscar Brännström Iverbring <oscarbrannstromiverbring@elev.ga.ntig.se>
2024-09-02 14:52:04 +0200

    tränat på if

---
+++ Kapitel 3/EngChart/Program.cs
@@ -0,0 +1,64 @@
+﻿// Program som simulerar kända Engineering Flowchart
+using System.Diagnostics.CodeAnalysis;
+
+Console.Clear();
+Console.WriteLine("Välkommen till ingenjörens flödesschema");
+
+// Visa en Ascii art
+Console.WriteLine($"""
+
+                       -   ,--_--.
+               -           \      `.
+                      -     "-_ _   \
+     -                         / F   )
+                   -     -    / / `--'
+              -              / /
+                   -        / /
+            -            __/ /
+                        /,-pJ
+           -        _--"-L ||
+                  ,"      "//
+     -           /  ,-"".//\
+                /  /     // J____
+               J  /     // L/----\
+   .           F J     //__//^---'
+     `     ___J  F    '----| |
+  `       J---|  F         F F
+`   `. `   `--J  L        J  F
+    .   .`     L J       J  F
+       .  .    J  \    ,"  F
+         .  `.` \  "--"  ,"
+    hs      ` ``."-____-"
+
+""");
+
+Console.Write("Does it move? (Y/N)");
+string answer = Console.ReadLine();
+
+// Kolla om svar är ja eller nej
+if (answer == "Y")
+{ // Y
+Console.Write("Should it? (Y/N)");
+answer = Console.ReadLine();
+if (answer == "Y")
+{
+    Console.Write("No problem!");
+}
+else
+{
+    Console.Write("Please use duct tape");
+}
+}
+else
+{ // N
+    Console.Write("Should it (Y/N)");
+    answer = Console.ReadLine();
+    if (answer == "Y")
+    {
+        Console.Write("Please use WD-40!");
+    }
+    else
+    {
+        Console.Write("No problem!");
+    }
+}
---
+++ Kapitel 3/InteractivStory/Program.cs
@@ -0,0 +1,49 @@
+﻿
+Console.Clear();
+Console.WriteLine("Welcome to an interactive story");
+
+// Dörr
+Console.WriteLine($"""
+There is a door in front of you. 
+""");
+
+Console.Write("Do you open the door? (Y/N)");
+string answer = Console.ReadLine();
+
+// Kolla om svar är ja eller nej
+if (answer == "Y")
+{ // Y
+Console.Write("A lit up room stands in front of you. Do you enter? (Y/N)");
+answer = Console.ReadLine();
+if (answer == "Y")
+{
+    Console.Write("Congratulations! There is cake for you!");
+}
+else
+{
+    Console.Write("You close the door and move on");
+}
+}
+else
+{ // N
+    Console.Write("Are you sure? (Y/N)");
+    answer = Console.ReadLine();
+    if (answer == "N")
+    {
+        Console.Write("You open the door. A lit up room stands in front of you. Do you enter? (Y/N)");
+        answer = Console.ReadLine();
+        if (answer == "Y")
+        {
+            Console.Write("Congratulations! There is cake for you!");
+        }
+        else
+        {
+            Console.Write("You close the door and move on. Pondering about what could of been behind the door");
+        }
+    }
+    else
+    {
+        Console.Write("You move on. Pondering about what could of been behind the door");
+    }
+}
+Console.WriteLine(" The End");
