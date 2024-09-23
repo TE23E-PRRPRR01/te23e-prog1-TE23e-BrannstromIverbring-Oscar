6d5a3231c75e4c6fa899297fa7de6665bf98af65
Oscar Brännström Iverbring <oscarbrannstromiverbring@elev.ga.ntig.se>
2024-09-23 14:49:13 +0200

    tränat på whil-loop och filhantering

---
+++ Kapitel 4/MenyProgram/Program.cs
@@ -0,0 +1,46 @@
+﻿// Program med meny
+Console.Clear();
+Console.WriteLine("Program med meny");
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+// Variabel för valet
+string val = "";
+
+// Program som snurrar  tills användaren säger avsluta
+
+while (true)
+{
+    // Visar en meny och läser användarens val
+    Console.Write("""
+    Välj ett av följande alternativ:
+    1. Omvandla en text till versaler
+    2. Omvandla en text till gemener
+    3. Avsluta programmet
+    """);
+
+    // Läs in användarens val
+    val = Console.ReadLine();
+
+    // Hantera användarens val
+    if (val == "1")
+    {
+        Console.Write("Skriv in en text:");
+        string texten = Console.ReadLine().ToUpper();
+        Console.WriteLine($"Texten i versaler blir: {texten}");
+    }
+    else if (val == "2")
+    {
+        Console.Write("Skriv in en text:");
+        string texten = Console.ReadLine().ToLower();
+        Console.WriteLine($"Texten i gemener blir: {texten}");
+    }
+    else if (val == "3")
+    {
+        Console.WriteLine("Tack för idag.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Du valde ett ogiltigt alternativ, försök igen.");
+    }
+}
---
+++ Kapitel 4/TextEditor/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.Clear();
+Console.WriteLine("""    
+    Texteditor
+    ============
+    """);
+// Variabel för valet
+string val = "";
+
+// Program som snurrar  tills användaren säger avsluta
+
+while (true)
+{
+    // Visar en meny och läser användarens val
+    Console.Write("""
+
+    Välj ett av följande alternativ:
+    1. Skriv till fil
+    2. Läs från fil
+    3. Avsluta programmet
+    """);
+
+    // Läs in användarens val
+    val = Console.ReadLine();
+
+    // Hantera användarens val
+    if (val == "1")
+    {
+        Console.WriteLine("Skriv in en text: ");
+        string text = Console.ReadLine();
+        File.WriteAllText("filnamn.txt", text);
+      
+    
+    }
+    else if (val == "2")
+    {
+        if (File.Exists("filnamn.txt"))
+        {
+        string text = File.ReadAllText("filnamn.txt");
+        Console.WriteLine(text);
+        }
+        else
+        {
+            Console.WriteLine("Filen finns inte.");
+        }
+        
+    }
+    else if (val == "3")
+    {
+        Console.WriteLine("Tack för idag.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Du valde ett ogiltigt alternativ, försök igen.");
+    }
+}
