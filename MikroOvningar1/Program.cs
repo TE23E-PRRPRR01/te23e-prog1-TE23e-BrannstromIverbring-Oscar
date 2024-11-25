ecd1ffa89aec46b1ea986363f4d615ab03608fba
Oscar Brännström Iverbring <oscarbrannstromiverbring@elev.ga.ntig.se>
2024-11-25 14:52:54 +0100

    list

---
+++ MikroOvningar1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Samlingar");
+
+List<string> listaBöcker = [];
+
+List<string> listaFilmer = ["2001", "I Robot", "Scarface"];
+
+Console.WriteLine($"Alla filmer: {string.Join(", ", listaFilmer)}");
+
+listaBöcker.Add("Pippi");
+Console.WriteLine($"Alla filmer: {string.Join(", ", listaBöcker)}");
+listaFilmer.Add("Black Witch");
+Console.WriteLine($"Alla filmer: {string.Join(", ", listaFilmer)}");
+
+Console.Write("Ange en bok: ");
+string nyBok = Console.ReadLine();
+listaBöcker.Add(nyBok);
+Console.WriteLine($"Alla filmer: {string.Join(", ", listaBöcker)}");
+
+if (listaBöcker.Contains("Bibel"))
+{
+    Console.WriteLine("Den finns");
+    listaBöcker.Remove("Bibel");
+}
+else
+{
+    Console.WriteLine("Den finns inte");
+}
