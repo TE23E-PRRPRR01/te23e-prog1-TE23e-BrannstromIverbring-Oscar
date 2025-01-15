408f4f09b3a933c4e50b70ccdaaef6b61121cf83
Oscar Brännström Iverbring <oscarbrannstromiverbring@elev.ga.ntig.se>
2025-01-15 15:04:55 +0100

    tränat på metoder som returnerar

---
+++ Kapitel 6/MetoderSomReturner/Program.cs
@@ -0,0 +1,46 @@
+﻿/* ***************************************
+*******************Main*******************
+**************************************** */
+Console.Clear();
+
+int svar = Addera(3, 5);
+Console.WriteLine($"Svaret är {svar}");
+Console.WriteLine(Addera(3, 5));
+Console.WriteLine($"3 + 5 = {Addera(3, 5)}");
+Addera(3, 5);
+
+string namn = "Oscar";
+Console.WriteLine($"{namn} baklänges är {VändText(namn)}");
+
+/* ***************************************
+*****************METODER******************
+**************************************** */
+
+static int Addera(int tal1, int tal2)
+{
+    int summa = tal1 + tal2;
+    return summa;
+}
+
+static string VändText(string text)
+{
+    string omvändText = "";
+    for (var i = text.Length - 1; i >= 0; i--)
+    {
+        omvändText += text[i];
+    }
+    return omvändText;
+}
+
+double Multiplicera(double tal1, double tal2)
+{
+    double summa = tal1 * tal2;
+    return summa;
+}
+
+double Dividera(double tal1, double tal2)
+{
+    double summa = tal1 / tal2;
+    return summa;
+}
+
